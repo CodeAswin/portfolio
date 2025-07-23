0c3522529c61d04ae1613647f430b092caae614d
Aswin <aswintptp0@gmail.com>
2025-07-23 17:48:11 +0530

    Updated portfolioData.ts

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -40,7 +40,7 @@ export const portfolioItems: WorkItem[] = [
     url: 'https://media.discordapp.net/attachments/1252290999363960897/1395034485828882583/edited.png?ex=6878fa9b&is=6877a91b&hm=99bc9608352953ae8733da6fe2ea387ac7a1cb985b918ddbdc7df8aff8492556&=&format=webp&quality=lossless',
     name: '3D Minecraft Thumbnail',
     isYouTubeVideo: false,
-    enabled: true
+    enabled: false
   },
   
   // Example YouTube Video
@@ -50,7 +50,7 @@ export const portfolioItems: WorkItem[] = [
     url: 'https://media.discordapp.net/attachments/1252290999363960897/1395034536508915844/Immotal_Thumbnail.png?ex=6878faa7&is=6877a927&hm=7cb969d497bda2c054c57b0a47b9a1ceb6c48242152c5e026b4767da39efca18&=&format=webp&quality=lossless',
     name: '3D Minecraft Thumbnail',
     isYouTubeVideo: false,
-    enabled: true
+    enabled: false
   },
   
   // Example Local Video (MP4)
@@ -60,7 +60,7 @@ export const portfolioItems: WorkItem[] = [
     url: 'https://media.discordapp.net/attachments/1252290999363960897/1395034508142706710/Friver_Thumbnail.png?ex=6878faa1&is=6877a921&hm=ed36362a3c24fd3b97cda6c1fc90585c3e21200d1c39f9326527b43cb125fe11&=&format=webp&quality=lossless&width=1460&height=821',
     name: '3D Minecraft Thumbnail',
     isYouTubeVideo: false,
-    enabled: true
+    enabled: false
   },
 
   {
@@ -69,7 +69,7 @@ export const portfolioItems: WorkItem[] = [
     url: 'https://media.discordapp.net/attachments/1252290999363960897/1395034566476955768/Fogedited.png?ex=6878faaf&is=6877a92f&hm=1903695d7ed799dc6502442580b532604137f82466550f85595c2d81434a54c0&=&format=webp&quality=lossless&width=1460&height=821',
     name: '3D Minecraft Foggy Render',
     isYouTubeVideo: false,
-    enabled: true
+    enabled: false
   },
 
   {
@@ -78,7 +78,7 @@ export const portfolioItems: WorkItem[] = [
     url: 'https://media.discordapp.net/attachments/1252290999363960897/1395034795444146287/abmweddd.png?ex=6878fae5&is=6877a965&hm=3145604929c8e6bddd0cec9875d99cfa38dec273e36bc36959a9a6e87e95b708&=&format=webp&quality=lossless&width=197&height=350',
     name: 'BMW Render',
     isYouTubeVideo: false,
-    enabled: true
+    enabled: false
   },
 
   {
@@ -87,7 +87,7 @@ export const portfolioItems: WorkItem[] = [
     url: 'https://media.discordapp.net/attachments/1252290999363960897/1395034816319197204/abmwed.png?ex=6878faea&is=6877a96a&hm=f422e6616c482dd3a3f4d1d4f3550682476d635097ed967bb8f78f48d13f21ae&=&format=webp&quality=lossless&width=520&height=924',
     name: 'BMW Render',
     isYouTubeVideo: false,
-    enabled: true
+    enabled: false
   },
 
   {
@@ -96,7 +96,7 @@ export const portfolioItems: WorkItem[] = [
     url: 'https://media.discordapp.net/attachments/1252290999363960897/1395035253948551179/BMW_after_ed.png?ex=6878fb53&is=6877a9d3&hm=5480ed6f7367c84d43a69ee38577f5c3b33ba8068da87e707cffc53b064fff6e&=&format=webp&quality=lossless&width=197&height=350',
     name: 'BMW Render',
     isYouTubeVideo: false,
-    enabled: true
+    enabled: false
   },
 
   {
@@ -105,7 +105,7 @@ export const portfolioItems: WorkItem[] = [
     url: 'https://media.discordapp.net/attachments/1252290999363960897/1395034472059240498/Clear_style_Edited.png?ex=6878fa98&is=6877a918&hm=2f9d60fbe24f509848740ce17faa08164e5d4e75174df2345487831c0ebd6fd2&=&format=webp&quality=lossless&width=925&height=925',
     name: 'Minecraft PFP',
     isYouTubeVideo: false,
-    enabled: true
+    enabled: false
   },
 
   // Example Google Drive Image (just paste the share URL directly):
